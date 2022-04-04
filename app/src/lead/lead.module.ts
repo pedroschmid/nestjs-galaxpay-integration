14f746ce5adabfcdbe346c7173714c543c08c477
Pedro Abreu <pedroschmid.yooga@gmail.com>
2022-04-04 16:45:46 -0300

    Added AddressRepository

--- app/src/lead/lead.module.ts
+++ app/src/lead/lead.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
-import { LeadRepository } from 'src/lead/lead.repository';
+import { LeadRepository } from 'src/lead/repositories/lead.repository';
 import { LeadController } from 'src/lead/lead.controller';
 import { LeadService } from 'src/lead/lead.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { GalaxpayService } from 'src/services/galaxpay/galaxpay.service';
-import { HttpModule } from '@nestjs/axios';
 
+import { AddressRepository } from 'src/lead/repositories/address.repository';
+
+import { GalaxpayService } from 'src/services/galaxpay/galaxpay.service';
 @Module({
   imports: [
-    TypeOrmModule.forFeature([LeadRepository]),
+    TypeOrmModule.forFeature([LeadRepository, AddressRepository]),
     HttpModule,
     ClientsModule.register([
       {
