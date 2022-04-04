8cd29a1e181725822b475f109ef7c3e736cc6a01
Pedro Abreu <pedroschmid.yooga@gmail.com>
2022-04-04 16:46:05 -0300

    Injected address repository

--- app/src/lead/lead.service.ts
+++ app/src/lead/lead.service.ts
@@ -2,35 +2,44 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { DeleteResult, UpdateResult } from 'typeorm';
 
 import { Lead } from 'src/lead/entities/lead.entity';
+import { Address } from 'src/lead/entities/address.entity';
 
-import { LeadRepository } from 'src/lead/lead.repository';
+import { LeadRepository } from 'src/lead/repositories/lead.repository';
+import { AddressRepository } from 'src/lead/repositories/address.repository';
 
 import { CreateLeadDTO } from 'src/lead/dtos/create-lead.dto';
 import { UpdateLeadDTO } from 'src/lead/dtos/update-lead.dto';
+
 import { ILeadService } from 'src/lead/interfaces/lead-service.interface';
 import { IParams } from 'src/lead/interfaces/params.interface';
 
 @Injectable()
 export class LeadService implements ILeadService {
-  constructor(private readonly leadRepository: LeadRepository) {}
+  constructor(
+    private readonly leadRepository: LeadRepository,
+    private readonly addressRepository: AddressRepository
+  ) {}
 
   public async findAll(params: IParams): Promise<Lead[]> {
-    return await this.leadRepository.findAll(params);
+    return await this.leadRepository.findAllLeads(params);
   }
 
   public async findById(id: string): Promise<Lead> {
-    return await this.leadRepository.findById(id);
+    return await this.leadRepository.findLeadById(id);
   }
 
-  public async store(payload: CreateLeadDTO): Promise<Lead> {
-    return await this.leadRepository.store(payload);
+  public async store(payload: CreateLeadDTO): Promise<CreateLeadDTO> {
+    const lead: Lead = await this.leadRepository.storeLead(payload);
+    const address: Address = await this.addressRepository.storeAddress(lead.id, payload.address);
+
+    return { ...lead, address };
   }
 
   public async update(
     id: string,
     payload: UpdateLeadDTO,
   ): Promise<UpdateResult> {
-    const lead: Lead = await this.leadRepository.findById(id);
+    const lead: Lead = await this.leadRepository.findLeadById(id);
 
     if (!lead) {
       throw new NotFoundException('Error while updating lead, not found!');
@@ -40,6 +49,6 @@ export class LeadService implements ILeadService {
   }
 
   public async destroy(id: string): Promise<DeleteResult> {
-    return await this.leadRepository.delete(id);
+    return await this.leadRepository.destroyLead(id);
   }
 }
